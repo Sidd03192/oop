304626b1f41208c31adafa43ad559bbcaea564ed
Sidd03192 <siddharthpotta19@gmail.com>
2026-03-03 17:23:12 -0600

    Final version fo tlb

--- tlb.sv
+++ tlb.sv
@@ -15,53 +15,62 @@ module tlb (
 
     logic [60:0] ways [16];
     logic [15:0] lrumat [16];
-    logic [3:0]  way_index;
-    logic        ready
+    logic [3:0]  hit_index;
+    logic [3:0]  free_index;
+    logic        hit_found;
+    logic        free_found;
 
+    always_comb begin
+        free_index = 4'b0;  // Default
+        hit_index =  4'b0;
+        hit_found =  1'b0;
+        free_found =  1'b0;
+        
+        for (int i = 0; i < 16; i++) begin
+            if (!hit_found && ways[i][60] && (ways[i][35:0] == vaddr[47:12])) begin
+                hit_found = 1'b1;
+                hit_index = i[3:0];
+            end
+            if (!free_found && (lrumat[i] == 16'b0)) begin
+                free_found = 1'b1;
+                free_index = i[3:0];
+            end
+        end
+    end
 
-
-    always @(posedge clk or negedge rst_n) begin:
+    always_ff @(posedge clk or negedge rst_n) begin
 
         if (!rst_n) begin
             // clear array and set everything to :(
-        end
-        
-        if (start) begin
+            ready <= 1;
+            valid <= 0;
+            panic_tlb_miss <= 0;
+            for (int i = 0; i < 16; i++) begin
+                ways[i] <= '0;
+                lrumat[i] <= '0;
+            end
+        end else if (start) begin
             ready <= 0;
             if (is_tlb_fill) begin
-                // do this
-                // get way to update 
-
-                // update lrumat -------------
-                // finding where to put
-                for (int i = 0; i < 16; i++) begin
-                    if(lrumat[i] == 16'b0) begin
-                        way_index <= i;
-                        break;
-                    end
-                end
-                
-                // update lru
-                lrumat[way_index] <= {16{1'b1}};
-                for(int j = 0 ; j < 16; j++) begin
-                    lrumat[j][way_index] <= 1'b0;
-                end
-                
                 // [v, paddr_tag, vaddr_tag], vaddr contains index and block
-                ways[way_index] <= {1'b1, paddr[29:6], vaddr[47:12]};
+                ways[free_index] <= {1'b1, paddr[29:6], vaddr[47:12]};
+                // update lrumat 
+                lrumat[free_index] <= {16{1'b1}};
+                for(int j = 0 ; j < 16; j++) begin
+                    lrumat[j][free_index] <= 1'b0;
+                end
+                
             end else begin
                 // tlb index
-                for (int i = 0; i < 16; i ++) begin
-                    // Check valid bit and if the virtual addresses match
-                    if (ways[i][60] && ways[i][35:0] == vaddr[47:12]) begin
-                        // TLB hit
-                        // {paddr, block offset}
-                        result_paddr <= {ways[i][59:36], vaddr[5:0]};
-                        break;
-                    end 
+                // Check valid bit and if the virtual addresses match (in always)
+                if (hit_found) begin
+                    result_paddr <= {ways[hit_index][59:36], vaddr[5:0]};
+                    panic_tlb_miss <= 1'b0;
+                end else begin
+                    panic_tlb_miss <= 1'b1;
                 end
             end
-            
+    
             valid <= 1;
         end else begin
             ready <= 1;
@@ -70,12 +79,3 @@ module tlb (
     end
 endmodule
 
-
-
-// // // // // // // //     for (int i = 0; i < 16; i ++) begin
-//         // whatever you do here\
-//         if(lrumat[i] == 16'b0) begin
-//             way_index = i;
-//             break;
-//         end
-//     end// // 
