be64f6a791a18aab96eb1ad306780729228ef01f
Max Feldman <mackx@protonmail.com>
2026-03-08 16:03:25 -0500

    wire up l2 and mshr inside of l1

--- l1.sv
+++ l1.sv
@@ -37,9 +37,6 @@ module l1_cache #(
     logic [L1_SETS-1:0] set_valids [L1_WAYS];
     logic [L1_SETS-1:0] set_dirty [L1_WAYS];
 
-    // mshr management
-    logic [NUM_MSHRS-1:0] mshr_ready; // TODO wire this up to the mshrs correctly
-
     /*
         States
             0 - Wait
@@ -56,7 +53,7 @@ module l1_cache #(
     // TAG CHECK COMBINATIONAL LOGIC
     // Tag from TLB
     logic [TAG_SIZE-1:0] incoming_tag;
-    assign incoming_tag = tlb_paddr[PA_WIDTH-1 -: TAG_SIZE];
+    assign incoming_tag = tlb_paddr_in[PA_WIDTH-1 -: TAG_SIZE];
     logic hit;
     logic [$clog2(L1_WAYS)-1:0] hit_way;
 
@@ -71,27 +68,120 @@ module l1_cache #(
         end
     end
 
-    // TODO add MSHR modules, L2 cache and wire up here
+    // for MSHR consumption (from L1)
+    logic                       mshr_miss_in[NUM_MSHRS];
+    logic[BLOCK_SIZE*8-1:0]     mshr_paddr_in[NUM_MSHRS];
+    logic                       mshr_wd_in[NUM_MSHRS];
+    logic                       mshr_superior_data_in[NUM_MSHRS];
+    logic                       mshr_evict_in[NUM_MSHRS];
+
+    // for MSHR consumption (from L2)
+    logic                       l2_resolve_out[NUM_MSHRS];
+    logic                       l2_superior_data_out[NUM_MSHRS];
+    logic                       l2_eack_out[NUM_MSHRS];
+
+    // for L1 consumption
+    logic                       mshr_empty_out[NUM_MSHRS];
+    logic                       mshr_pending_einfo_out[NUM_MSHRS];
+
+    // for L2 consumption
+    logic                       mshr_pending_res_out[NUM_MSHRS];
+    logic                       mshr_pending_eack_out[NUM_MSHRS];
+    logic[PA_WIDTH-1:0]         mshr_paddr_out[NUM_MSHRS];
+    logic                       mshr_wd_out[NUM_MSHRS];
+    logic[BLOCK_SIZE*8-1:0]     mshr_data_out[NUM_MSHRS];
+
+    l2_cache l2 (
+        .clk(clk),
+        .rst_n(rst_n),
+        
+        // input from L1-L2 MSHRs
+        .pending_res_in(mshr_pending_res_out),
+        .pending_eack_in(mshr_pending_eack_out),
+        .paddr_in(mshr_paddr_out),
+        .wd_in(mshr_data_out),
+        .data_in(mshr_data_out),
+
+        // output to L1-L2 MSHRs
+        .resolve_out(l2_resolve_out),
+        .superior_data_out(l2_superior_data_out),
+        .eack_out(l2_eack_out)
+    );
+
+    generate
+        for (genvar i = 0; i < NUM_MSHRS; i++) begin
+            MSHR #(PA_WIDTH, BLOCK_SIZE) m (
+                .clk_in(clk),
+                .rst_n(rst_n),
+
+                // input from L1
+                .miss_in(mshr_miss_in[i]),
+                .paddr_in(mshr_paddr_in[i]),
+                .wd_in(mshr_wd_in[i]),
+                .superior_data_in(mshr_superior_data_in[i]),
+                .evict_in(mshr_evict_in[i]),
+
+                // input from L2
+                .resolve_in(l2_resolve_out[i]),
+                .inferior_data_in(l2_superior_data_out[i]),
+                .eack_in(l2_eack_out[i]),
+
+                // output for L1
+                .empty_out(mshr_empty_out[i]),
+                .pending_einfo_out(mshr_pending_einfo_out[i]),
+
+                // output for L2
+                .pending_res_out(mshr_pending_res_out[i]),
+                .pending_eack_out(mshr_pending_eack_out[i]),
+                .paddr_out(mshr_paddr_out[i]),
+                .wd_out(mshr_wd_out[i]),
+
+                // ouput for both
+                .data_out(mshr_data_out[i])
+            );
+        end
+    endgenerate
+
 
     // TODO finish implementing where the comments in the always block say
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-
+            // reset 
+            set_contents <= '0;
+            tags <= '0;
+            lru <= '0;
+            set_valids <= '0;
+            set_dirty <= 0'
+            state <= '0;
+            cur_index <= '0;
+            cur_w <= '0;
+            cur_wdata <= '0;
         end else begin
-            // check MSHR events, potentially unstall if stalling
+
+            generate
+                for (genvar i = 0; i < NUM_MSHRS; i++) begin
+                    // check MSHR[i]
+                    if (mshr_pending_einfo_out[i]) begin
+                        // evict, tell mshr
+                    end
+                end
+            endgenerate
+
 
             if (state == 0) begin
                 // handle input, advance state
-                state <= 1;
+                state = 1;
 
             end else if (state == 1) begin
                 // got paddr, now see if we have a hit, advance state
-                state <= 2;
+                state = 2;
                 if (hit) begin
-                    // DO LRU on set
+                    // DO LRU on set, (write & set dirty)
                 end else begin
                     // miss, go to MSHR
                 end
+            end else if (state == 2) begin
+                // unstall if an mshr is now empty
             end
         end
 
--- mshr.sv
+++ mshr.sv
@@ -29,7 +29,7 @@ module MSHR #(
     output  logic                           pending_einfo_out,
 
     // output for inferior memory
-    output  logic                           pending_resolution_out,
+    output  logic                           pending_res_out,
     output  logic                           pending_eack_out,
     output  logic[PA_WIDTH-1:0]             paddr_out,
     output  logic                           wd_out,
@@ -42,7 +42,7 @@ module MSHR #(
     always_ff @(posedge clk_in or negedge rst_n) begin
         if (!rst_n) begin
             empty_out <= 1
-            pending_resolution_out <= 0
+            pending_res_out <= 0
             pending_einfo_out <= 0;
             pending_eack_out <= 0;
             wd_out <= 0;
@@ -51,7 +51,7 @@ module MSHR #(
             if (miss_in) begin
                 state <= 1;
                 empty_out <= 0;
-                pending_resolution_out <= 1;
+                pending_res_out <= 1;
                 paddr_out <= paddr_in;
                 wd_out <= wd_in;
                 data_out <= superior_data_in;
@@ -59,23 +59,18 @@ module MSHR #(
         end else if (state == 1) begin
             if (resolve_in) begin
                 state <= 2;
-                pending_resolution_out <= 0;
+                pending_res_out <= 0;
                 pending_einfo_out <= 1;
                 data_out <= inferior_data_in;
              end
         end else if (state == 2) begin
             if (evict_in) begin
                 pending_einfo_out <= 0;
+                pending_eack_out <= 1;
+                state <= 3;
                 paddr_out <= paddr_in;
                 wd_out <= wd_in;
-                if (wd_in) begin
-                    state <= 3;
-                    data_out <= superior_data_in;
-                    pending_eack_out <= 1;
-                end else begin
-                    state <= 0;
-                    empty_out <= 1;
-                end
+                data_out <= superior_data_in;
             end
         end else if (state == 3) begin
             if (eack_in) begin
